e1d0b9d38144e6b016d21c734ddd1aea86226aac
debrian <debrianfigueroa@gmail.com>
2017-01-21 10:59:30 -0500

    Version 4.211 Character model created

--- app/src/main/java/com/athon/hack/ddcompanion/CustomPartyListAdapter.java
+++ app/src/main/java/com/athon/hack/ddcompanion/CustomPartyListAdapter.java
@@ -5,6 +5,7 @@ import android.widget.ArrayAdapter;
 
 
 public class CustomPartyListAdapter extends ArrayAdapter {
+
     public CustomPartyListAdapter(Context context, int resource) {
         super(context, R.layout.party_row);
     }
