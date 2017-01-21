f37ee56b8ea91a39d3b0cbb731378fac95811cb1
Loslylee <lin.isabella.li@gmail.com>
2017-01-21 11:35:43 -0500

    Add files via upload

---
+++ app/src/main/java/Character_Register.java
@@ -0,0 +1,13 @@
+package com.example.isabellalee.swamphacks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Character_Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character__register);
+    }
+}
